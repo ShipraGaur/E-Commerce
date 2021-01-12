e1628133aefcd8abc808bfc9fd3160a32cb2e733
Shipra Gaur <sgshipragaur@gmail.com>
2021-01-13 01:31:32 +0530

    JWT Token added for auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler' 
-import User from '../models/userModel.js '
+import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc   Authenticate user & get token 
 // @route  POST/api/users/login
@@ -15,7 +16,7 @@ export const authUser = asyncHandler(async (req,res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     }
     else{
@@ -24,3 +25,22 @@ export const authUser = asyncHandler(async (req,res) => {
     }
 })
 
+// @desc   Get user Profile 
+// @route  GET/api/users/profile
+// @access Private
+export const getUserProfile = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    }
+    else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { authUser } from '../controllers/UserController.js'
+import { authUser, getUserProfile} from '../controllers/UserController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
