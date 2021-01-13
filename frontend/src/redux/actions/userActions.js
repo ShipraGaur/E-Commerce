3d90144636deb5e4c16777300b6bc566df717b6c
Shipra Gaur <sgshipragaur@gmail.com>
2021-01-14 03:06:26 +0530

    Profile screen with get and update user details functionality added

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -68,3 +68,72 @@ export const register = (name, email, password) => async(dispatch) => {
         }) 
     }
 }
+
+export const getUserDetails = (id) => async(dispatch, getState) => {
+    try { 
+        dispatch({ type: ActionTypes.USER_DETAILS_REQUEST })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.get(`/api/users/${id}`, config)
+
+        dispatch({ 
+            type: ActionTypes.USER_DETAILS_SUCCESS,
+            payload: data
+        })
+    } 
+    catch (error) {
+        dispatch({ 
+            type: ActionTypes.USER_DETAILS_FAIL,
+            payload: error.response && error.response.data.message 
+                        ? error.response.data.message 
+                        : error.message
+        }) 
+    }
+}
+
+export const updateUserProfile = (user) => async(dispatch, getState) => {
+    try { 
+        dispatch({ type: ActionTypes.USER_UPDATE_PROFILE_REQUEST })
+
+        const { userLogin: { userInfo } } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const {data} = await axios.put(`/api/users/profile`, user, config)
+
+        dispatch({ 
+            type: ActionTypes.USER_UPDATE_PROFILE_SUCCESS,
+            payload: data
+        })
+        dispatch({
+            type: ActionTypes.USER_LOGIN_SUCCESS,
+            payload: data,
+          })
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } 
+    catch (error) {
+        const message = error.response && error.response.data.message
+                        ? error.response.data.message
+                        : error.message
+            if (message === 'Not authorized, token failed') {
+                dispatch(logout())
+            }
+            dispatch({
+                type: ActionTypes.USER_UPDATE_PROFILE_FAIL,
+                payload: message,
+            })
+    }
+}
--- frontend/src/redux/reducers/userReducers.js
+++ frontend/src/redux/reducers/userReducers.js
@@ -28,3 +28,30 @@ export const userRegisterReducer = (state = {}, action) => {
     }
 }
 
+export const userDetailsReducer = (state = { user: { }}, action) => {
+    switch(action.type){
+        case ActionTypes.USER_DETAILS_REQUEST:
+            return { ...state, loading: true}
+        case ActionTypes.USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+        case ActionTypes.USER_DETAILS_FAIL:
+            return { loading:false, error: action.payload}
+        default:
+            return state
+    }
+}
+
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+    switch(action.type){
+        case ActionTypes.USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true}
+        case ActionTypes.USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload }
+        case ActionTypes.USER_UPDATE_PROFILE_FAIL:
+            return { loading:false, error: action.payload}
+        default:
+            return state
+    }
+}
+
