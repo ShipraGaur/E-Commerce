a83dcdbfd7321ef542a3e93ac270ef9b15c5f137
Shipra Gaur <sgshipragaur@gmail.com>
2021-01-18 16:31:34 +0530

    Added PayPal Payment

--- frontend/src/redux/reducers/orderReducers.js
+++ frontend/src/redux/reducers/orderReducers.js
@@ -24,6 +24,21 @@ export const orderDetailsReducer = (state = { orderItems: [], shippingAddress: {
             return { loading:false, error: action.payload}
         default:
             return state
+    }
+}
+
+export const orderPayReducer = (state = {}, action) => {
+    switch(action.type){
+        case ActionTypes.ORDER_PAY_REQUEST:
+            return { loading: true }
+        case ActionTypes.ORDER_PAY_SUCCESS:
+            return { loading: false, success: true, order: action.payload }
+        case ActionTypes.ORDER_PAY_FAIL:
+            return { loading:false, error: action.payload}
+        case ActionTypes.ORDER_PAY_RESET:
+            return {}
+        default:
+            return state
 
     }
 }
